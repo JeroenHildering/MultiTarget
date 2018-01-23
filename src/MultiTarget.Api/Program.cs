648fd46c263d63990c2b401d94b5705d273ea30a
Jeroen Hildering <j.hildering@vanlanschot.com>
2018-01-23 15:00:15 +0100

    Initial commit

---
+++ src/MultiTarget.Api/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace MultiTarget.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables("ASPNETCORE_")
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseKestrel(options =>
+                {
+                    options.AddServerHeader = false;
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
